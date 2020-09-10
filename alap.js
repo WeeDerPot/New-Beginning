782152c38bfd4e3f1abc1b682e237cfd92344e34
WeeDerPot <nemeth.christopher@sob.lidice.local>
2020-09-10 09:27:16 +0200

    Harmadik Commit

--- alap.js
+++ alap.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let nev = 'Németh Christopher';
+
+// Gyengén típusos nyelv
+/* Modális ablak függvények */
+// window.alert(nev);
+// alert(nev);
+
+// let tanuloszam = window.prompt('Hány tanuló van jelen?', 12);
+// let szoveg = `A tanulók száma: ${tanuloszam}`;
+
+let szam = 12335436565656565476573486763857658356764833567n * 2n;
+let szoveg = 'Szia bácsi';
+//szoveg = null;
+// window.alert(szam);
+
+document.getElementById('kiir').innerText = szoveg;
